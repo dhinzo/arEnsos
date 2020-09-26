6c38aa7cead462a992845aecb6245082f1caf9c1
Devin Hinzo <63556123+dhinzo@users.noreply.github.com>
2020-09-26 13:43:57 -0400

    AR test

---
+++ script.js
@@ -0,0 +1,41 @@
+window.onload = () => {
+  let places = staticLoadPlaces()
+  renderPlaces(places)
+}
+
+function staticLoadPlaces() {
+  return [
+    {
+      name: "AR Sculpture",
+      location: {
+        lat: 41.497917,
+        lng: -81.570093,
+      },
+    },
+  ]
+}
+
+function renderPlaces(places) {
+  let scene = document.querySelector("a-scene")
+
+  places.forEach((place) => {
+    let latitude = place.location.lat
+    let longitude = place.location.lng
+
+    let model = document.createElement("a-entity")
+    model.setAttribute(
+      "gps-entity-place",
+      `latitude: ${latitude}; longitude: ${longitude};`
+    )
+    model.setAttribute("gltf-model", "./assets/Red_Enso_3Rings2.glb")
+    model.setAttribute("rotation", "0 180 0")
+    model.setAttribute("animation-mixer", "")
+    model.setAttribute("scale", "0.5 0.5 0.5")
+
+    model.addEventListener("loaded", () => {
+      window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"))
+    })
+
+    scene.appendChild(model)
+  })
+}
