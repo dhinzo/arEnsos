f7c4e0bb90304fda64efa615fe5de396ddfee453
Devin Hinzo <63556123+dhinzo@users.noreply.github.com>
2020-10-03 13:34:28 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 window.onload = () => {
+  const button = document.querySelector('button[data-action="change"]')
+  button.innerText = "Cycle"
+
   let places = staticLoadPlaces()
   renderPlaces(places)
 }
@@ -6,13 +9,54 @@ window.onload = () => {
 function staticLoadPlaces() {
   return [
     {
-      name: "AR Sculpture",
+      name: "AR Sculptures",
       location: {
-        lat: 41.497726,
-        lng: -81.570082,
+        lat: 41.497716,
+        lng: -81.570081,
       },
     },
   ]
+}
+
+var models = [
+  {
+    url: "./assets/unaligned.glb",
+    scale: "0.5 0.5 0.5",
+    info: "Red Enso with Two Rotations",
+    rotation: "0 180 0",
+  },
+  {
+    url: "./assets/aligned.glb",
+    scale: "0.2 0.2 0.2",
+    rotation: "0 180 0",
+    info: "Red Enso with Three Rotations",
+  },
+  {
+    url: "./assets/color_enso.glb",
+    scale: "0.08 0.08 0.08",
+    rotation: "0 180 0",
+    info: "Red-colored Enso",
+  },
+]
+
+var modelIndex = 0
+var setModel = function(model, entity) {
+  if (model.scale) {
+    entity.setAttribute("scale", model.scale)
+  }
+
+  if (model.rotation) {
+    entity.setAttribute("rotation", model.rotation)
+  }
+
+  if (model.position) {
+    entity.setAttribute("position", model.position)
+  }
+
+  entity.setAttribute("gltf-model", model.url)
+
+  const div = document.querySelector(".instructions")
+  div.innerText = model.info
 }
 
 function renderPlaces(places) {
@@ -27,14 +71,19 @@ function renderPlaces(places) {
       "gps-entity-place",
       `latitude: ${latitude}; longitude: ${longitude};`
     )
-    model.setAttribute("gltf-model", "./assets/Red_Enso_3Rings2.glb")
-    // model.setAttribute("rotation", "0 180 0")
-    model.setAttribute("animation-mixer", "")
-    model.setAttribute("scale", "10 10 10")
 
-    model.addEventListener("loaded", () => {
-      window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"))
-    })
+    setModel(models[modelIndex], model)
+
+    model.setAttribute("animation-mixer", "")
+
+    document
+      .querySelector('button[data-action="change"]')
+      .addEventListener("click", function() {
+        var entity = document.querySelector("[gps-entity-place]")
+        modelIndex++
+        var newIndex = modelIndex % models.length
+        setModel(models[newIndex], entity)
+      })
 
     scene.appendChild(model)
   })
